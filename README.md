483c8efc6093be184eb47797e783da94b2831b35
amritpalsinghh <amritpal.linux@gmail.com>
2020-04-05 02:33:29 +0530

    Basic Delete fuction is added here just

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # linklist
 This is test project to learn git use in project development with teamwork
+Function: void delet(); 
+Comment : Helps to delet a node
--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 int main()
 {
 	printf("__Start: %s __\r\n",__func__);
+	delet();
 	printf("__Ends : %s __\r\n",__func__);
 	return 0;
+}
+void delet()
+{
+	printf("__Start: %s __\r\n",__func__);
+	printf("__Ends : %s __\r\n",__func__);
 }
