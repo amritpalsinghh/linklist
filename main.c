2e94cbfa1be596c30da537d6c32e72b8e121bf68
amritpalsinghh <amritpal.linux@gmail.com>
2020-04-05 02:26:44 +0530

    Just Create fuction body of ll is writen just

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 int main()
 {
 	printf("__Start: %s __\r\n",__func__);
+	create();
 	printf("__Ends : %s __\r\n",__func__);
 	return 0;
+}
+void create()
+{
+	printf("__Start: %s __\r\n",__func__);
+	printf("__Ends : %s __\r\n",__func__);	
 }
