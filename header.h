abff3792ef44b3f31dd0fe55756dd920d6788574
amritpalsinghh <amritpal.linux@gmail.com>
2020-04-05 03:02:39 +0530

    Create a Node, Create Node Structure

--- header.h
+++ header.h
@@ -1,2 +1,7 @@
 #include<stdio.h>
-void create();
+#include<stdlib.h>
+typedef struct LL_STR{
+	int a;
+	struct LL_STR * next;
+}LL_STR;
+LL_STR * create();
--- main.c
+++ main.c
@@ -2,12 +2,19 @@
 int main()
 {
 	printf("__Start: %s __\r\n",__func__);
-	create();
+	LL_STR * start;
+	start = NULL;
+	start=create();
 	printf("__Ends : %s __\r\n",__func__);
 	return 0;
 }
-void create()
+LL_STR * create()
 {
 	printf("__Start: %s __\r\n",__func__);
-	printf("__Ends : %s __\r\n",__func__);	
+	LL_STR * ob;
+	ob= (LL_STR *)malloc(sizeof(LL_STR));
+	ob->a=0;
+	ob->next=NULL;
+	printf("__Ends : %s __\r\n",__func__);
+	return ob;
 }
